d29cc1e0b53d74160784ff1313226a771f074a35
Junga <Jangomeirbek1324@gmail.com>
2026-07-04 15:54:51 +0500

    Add github repositories collection

--- scripts/collect_user.py
+++ scripts/collect_user.py
@@ -8,8 +8,15 @@ username = sys.argv[1]
 
 client = GitHubClient()
 data = client.get_user(username)
+repos = client.get_repositories(username)
 
 print(f"Username: {data['login']}")
 print(f"Name: {data['name']}")
 print(f"Followers: {data['followers']}")
 print(f"Public repos: {data['public_repos']}")
+for repo in repos: # loops through all the reposistories accessed by the requests (github rest api)
+    print(f"Repository Name: {repo['name']}")
+    print(f"Language: {repo['language']}")
+    print(f"Stars: {repo['stargazers_count']}")
+    print("-" * 30)
+
--- src/gitscore/github/client.py
+++ src/gitscore/github/client.py
@@ -16,3 +16,16 @@ class GitHubClient:
             raise Exception(f"GitHub request failed: {response.status_code}")
 
         return response.json()
+    def get_repositories(self, username):
+        url = f"https://api.github.com/users/{username}/repos"
+        headers = { "Accept": "application/json", "X-Requested-With": "XMLHttpRequest"}
+        headers = {}
+        if GITHUB_TOKEN:
+            headers["Authorization"] = f"Bearer {GITHUB_TOKEN}"
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code != 200:
+            raise Exception(f"GitHub repositories request failed: {response.status_code}")
+        
+        return response.json()
